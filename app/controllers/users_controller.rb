7f0ebb30b2a2293c705d994694795a37caa0736b
Kira McLean <kiraemclean@gmail.com>
2015-12-11 14:04:37 -0500

    Add login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
       render 'new'
@@ -20,6 +21,9 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :password,
+                                   :password_confirmation)
     end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,28 +1,29 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "should not create new user with form errors" do
+  test 'should not create new user with form errors' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name: "",
-                               email: "user@invalid",
-                               password: "foo",
-                               password_confirmation: "bar" }
+      post users_path, user: { name: '',
+                               email: 'user@invalid',
+                               password: 'foo',
+                               password_confirmation: 'bar' }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
   end
 
-  test "should create new user with valid form" do
+  test 'should create new user with valid form' do
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name: "Example",
-                                            email: "user@valid.example",
-                                            password: "testing",
-                                            password_confirmation: "testing" }
+      post_via_redirect users_path, user: { name: 'Example',
+                                            email: 'user@valid.example',
+                                            password: 'testing',
+                                            password_confirmation: 'testing' }
     end
     assert_template 'users/show'
     assert_not flash.empty?
+    assert is_logged_in?
   end
 end
