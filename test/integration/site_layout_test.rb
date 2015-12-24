38288d2eeb60575a7b22ef9b901212d7827ae963
Kira McLean <kiraemclean@gmail.com>
2015-12-24 10:24:13 -0400

    Finish chapter 9

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test 'layout links' do
+
+  def setup
+    @admin = users(:kira)
+  end
+
+  test 'layout links while not logged in' do
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
@@ -9,5 +14,18 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
     assert_select 'a[href=?]', signup_path
+    assert_select 'a[href=?]', login_path
+  end
+
+  test 'layout links as logged-in user' do
+    log_in_as(@admin)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', logout_path
   end
 end
