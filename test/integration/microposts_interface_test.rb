4e722967aeb0283ec516e4f4f775ef4ac317e547
Kira McLean <kiraemclean@gmail.com>
2016-01-22 16:17:12 -0500

    Add tests for feed and image upload

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -8,8 +8,9 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
   test 'micropost interface' do
     log_in_as @user
-    get root_url
+    get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, micropost: { content: '' }
@@ -17,9 +18,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid submission
     content = 'This is a micropost test'
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
+    micropost = @user.microposts.create!(content: content, picture: picture)
+    assert micropost.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -32,5 +36,20 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # Visit a different user
     get user_path(users(:mike))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test 'micropost sidebar count' do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    # User with zero microposts
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match '0 microposts', response.body
+    # Check pluralization of micropost
+    other_user.microposts.create!(content: 'A micropost')
+    get root_path
+    assert_match '1 micropost', response.body
   end
 end
